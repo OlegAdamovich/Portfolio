4cfe22715015ec27ca758af67c9604a59f5f079b
OlegAdamovich <oleg.adamovich@yahoo.com>
2020-09-01 12:54:30 +0300

    Updates

---
+++ precache-manifest.2eed1cdc189af5dabd83f5600533e0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9cef2903160e5c817ea3a3180e9396a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ab1f342a13872fe75805",
+    "url": "/Portfolio/static/css/main.26b7a227.chunk.css"
+  },
+  {
+    "revision": "a51a8827f8abd64aa0aa",
+    "url": "/Portfolio/static/js/2.30126f5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.30126f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1f342a13872fe75805",
+    "url": "/Portfolio/static/js/main.8a8b444b.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
